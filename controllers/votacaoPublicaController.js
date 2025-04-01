d12cf3c1e5dd680a9ca3957a583cf1696c7fa6e1
rafdavis <rd506269@gmail.com>
2025-04-01 20:47:31 -0300

    cleanup: remove some comments

--- controllers/votacaoPublicaController.js
+++ controllers/votacaoPublicaController.js
@@ -2,10 +2,10 @@ import votacaoPublicaService from "../services/VotacaoPublicaService.js";
  
 async function registrarVoto(id_candidato, id_participante, id_evento) {
   try {
-    // Inserir voto no banco de dados
+
     const query = `insert into Votos (id_candidato, id_participante, id_evento) values ($1, $2, $3)`
     const result = await pool.query(query, [id_candidato, id_participante, id_evento])
-    return result.rows[0] // Retornar o voto inserido
+    return result.rows[0] 
   } catch (error) {
     console.error(error)
   }
